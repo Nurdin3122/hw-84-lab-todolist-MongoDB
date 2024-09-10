e97834dba461b948710ab16de75542bcf13a6387
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-09-10 16:33:16 +0600

    Backend: добавлены новые маршруты для сущности Tasks, такие как PUT и DELETE. PUT редактирует уже имеющийся объект задачи у определенного пользователя, то есть другой пользователь не может изменить вашу задачу. DELETE также проверяет пользователя и удаляет задачу у пользователя.

--- Backend/routers/taskRouter.ts
+++ Backend/routers/taskRouter.ts
@@ -35,4 +35,56 @@ taskRouter.post("/",auth,async  (req,res) => {
         return res.status(400).send(error);
     }
 });
+
+
+taskRouter.delete("/:id", auth, async (req, res) => {
+    const user = (req as RequestWithUser).user;
+    const taskId = req.params.id;
+
+    try {
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+
+            return res.status(404).send({ error: 'Task not found' });
+
+        }
+
+        if (task.user.toString() !== user._id.toString()) {
+
+            return res.status(403).send({ error: 'You can only delete your own tasks' });
+        }
+
+        await Task.deleteOne({ _id: taskId });
+        return res.status(200).send({ message: 'Task deleted successfully' });
+    } catch (error) {
+        return res.status(500).send({ error: 'Failed to delete task' });
+    }
+});
+
+
+taskRouter.put("/:id", auth, async (req, res) => {
+    const user = (req as RequestWithUser).user;
+    const taskId = req.params.id;
+    const changedTask = req.body;
+
+    try {
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+
+            return res.status(404).send({ error: 'Task not found' });
+        }
+
+        if (task.user.toString() !== user._id.toString()) {
+
+            return res.status(403).send({ error: 'You can only edit your own tasks' });
+        }
+
+        const newTask = await Task.findByIdAndUpdate(taskId, changedTask, { new: true });
+        return res.status(200).send(newTask);
+    } catch (error) {
+        return res.status(400).send({ error: 'Failed to update task' });
+    }
+});
 export default taskRouter
